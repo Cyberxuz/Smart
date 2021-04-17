0a9e385cd7f62c79583a837933bdf6abae20b0ab
Fourcade7 <fourcadeaminov7@gmail.com>
2021-04-17 21:03:10 +0500

    database login register succes

--- app/src/main/java/com/example/smartshop/MainActivity.java
+++ app/src/main/java/com/example/smartshop/MainActivity.java
@@ -1,17 +1,54 @@
 package com.example.smartshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText textInputEditText1,textInputEditText2;
+    Button button;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button1);
+        textInputEditText1=findViewById(R.id.edittext1);
+        textInputEditText2=findViewById(R.id.edittext2);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signInWithEmailAndPassword(textInputEditText1.getText().toString(),textInputEditText2.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            startActivity(new Intent(MainActivity.this,MainActivity3.class));
+
+                        }else {
+                            Toast.makeText(MainActivity.this, "Kuda ketasan", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
