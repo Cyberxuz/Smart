f5407f46943a906e4d612fe0535c6f2f30458eed
Fourcade7 <fourcadeaminov7@gmail.com>
2021-04-17 20:42:03 +0500

    register bo`lding

--- app/src/main/java/com/example/smartshop/MainActivity2.java
+++ app/src/main/java/com/example/smartshop/MainActivity2.java
@@ -1,13 +1,22 @@
 package com.example.smartshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
 
     TextInputEditText textInputEditText1,textInputEditText2,textInputEditText3,textInputEditText4,textInputEditText5;
     Button button;
@@ -22,5 +31,20 @@ public class MainActivity2 extends AppCompatActivity {
         textInputEditText3=findViewById(R.id.edittext33);
         textInputEditText4=findViewById(R.id.edittext44);
         textInputEditText5=findViewById(R.id.edittext55);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.createUserWithEmailAndPassword(textInputEditText1.getText().toString(),textInputEditText4.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(MainActivity2.this, "Registratsiya bo`lding axir", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
     }
 }
