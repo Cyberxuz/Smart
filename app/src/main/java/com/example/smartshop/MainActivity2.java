8f3576f1eea57ef57c828093ac0e6d12f55e7484
Cyberxuz <otash2280gmail.com>
2021-04-17 20:34:20 +0500

    add

--- app/src/main/java/com/example/smartshop/MainActivity2.java
+++ app/src/main/java/com/example/smartshop/MainActivity2.java
@@ -2,13 +2,29 @@ package com.example.smartshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        button=findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+                finish();
+                Toast.makeText(MainActivity2.this, "Added", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
